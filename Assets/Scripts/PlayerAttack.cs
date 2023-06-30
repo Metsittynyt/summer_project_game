b2d1b07632ba81cc4d8bb5da220ab7383ec4124e
Elsa Lehtilä <elsa.v.lehtila@utu.fi>
2023-06-30 10:13:24 +0300

    player attack animation fixed

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -10,36 +10,49 @@ public class PlayerAttack : MonoBehaviour
 
     public float attackRange = 0.5f;
     public int attackDamage = 3;
+    public float attackRate = 2f;
 
+    float newxtAttackTime = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")) {
-            Attack();
+        if (Time.time >= newxtAttackTime)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Attack();
+                newxtAttackTime = Time.time + 1f / attackRate;
+            }
         }
     }
 
-    void Attack() {
+    void Attack()
+    {
         // Play attack animation
         animator.SetTrigger("Attack");
 
         // Deteck enemies in range of attack
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackArea.position, attackRange, enemyLayers);
-        
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(
+            attackArea.position,
+            attackRange,
+            enemyLayers
+        );
+
         // Damage enemies
-        foreach(Collider2D enemy in hitEnemies) {
+        foreach (Collider2D enemy in hitEnemies)
+        {
             Debug.Log("Player hit " + enemy.name + "!");
             enemy.GetComponent<Health>().Damage(attackDamage);
         }
-        
     }
 
-    void OnDrawGizmosSelected() {
-        if (attackArea == null) {
+    void OnDrawGizmosSelected()
+    {
+        if (attackArea == null)
+        {
             return;
         }
         Gizmos.DrawWireSphere(attackArea.position, attackRange);
-
     }
 }
